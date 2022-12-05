facc7c2942da74d0da1ca515df55b9e3b53a16ae
Ong Geng Kai <2100503h@student.tp.edu.sg>
2022-12-06 00:25:09 +0800

    jump and run animations

--- GDP/Assets/Scripts/Movement.cs
+++ GDP/Assets/Scripts/Movement.cs
@@ -9,7 +9,6 @@ public class Movement : MonoBehaviour
     public float speed;
     public float jump_height;
     private Rigidbody2D body;
-    //private Animator anim;
     public float jumpRate = 2f;
     float nextjumpTime = 0f;
     public bool grounded;
@@ -18,17 +17,26 @@ public class Movement : MonoBehaviour
     public GameObject[] plantArea = new GameObject[3];
     public string inventory;
     public TextMeshProUGUI inventext;
-
+    public Animator playerAnim;
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         inventext.text = null;
-        //anim = GetComponent<Animator>();
+        playerAnim = GetComponent<Animator>();
+
     }
 
     private void Update()
     {
-        if(!interacting)
+
+        if (Input.GetButtonDown("Horizontal") || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            playerAnim.Play("Run");
+        }
+
+
+
+        if (!interacting)
         {
             float horizontalInput = Input.GetAxis("Horizontal");
             body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
@@ -38,6 +46,8 @@ public class Movement : MonoBehaviour
             else if (horizontalInput < -0.01f)
                 transform.localScale = new Vector3(-0.15f, 0.15f, 0.15f);
 
+            
+
 
 
             if (Time.time >= nextjumpTime)
@@ -45,10 +55,9 @@ public class Movement : MonoBehaviour
                 if (Input.GetKey(KeyCode.Space) && grounded)
                 {
                     Jump();
+                    playerAnim.Play("Jump");         
                     nextjumpTime = Time.time + 6f / jumpRate;
                 }
-
-
             }
         }
     }
@@ -59,8 +68,7 @@ public class Movement : MonoBehaviour
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, jump_height);
-        //anim.SetTrigger("Jump");
+        body.velocity = new Vector2(body.velocity.x, jump_height);  
         grounded = false;
     }
     public void OnCollisionEnter2D(Collision2D collision)
