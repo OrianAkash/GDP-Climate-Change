35af7ec7eb4be8a4bdf7e44d3305327998968231
OrianAkash <kaungthantwinsg@gmail.com>
2023-02-05 18:14:56 +0800

    made some changes to dash, level, cutscenes

--- GDP/Assets/Scenes/Scripts/PestBox.cs
+++ GDP/Assets/Scenes/Scripts/PestBox.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PestBox : MonoBehaviour
 {
     public Health playerHealth;
+    public Movement dash;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,17 @@ public class PestBox : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        playerHealth.TakeDamage(0.5f);
+        if (collision.transform.tag == "Player")
+        {
+            if (dash.isDashing == true)
+            {
+
+            }
+            else
+            {
+                playerHealth.TakeDamage(0.5f);
+            }
+        }
+                
     }
 }
